747a6495274f93317a2d295e02643d5fc133c27d
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-08-03 16:06:12 -0300

    ajustes na apresentação do projeto feedbacks

--- src/components/MyProjects/data.ts
+++ src/components/MyProjects/data.ts
@@ -2,14 +2,14 @@
 import { MyProjectsListProps } from './types'
 import dashGifImage from '../../assets/gif/admin-dash.gif'
 import taskManagerGif from '../../assets/gif/taskManager-dash.gif'
-
+import feedbackDemonstrationGif from '../../assets/gif/gif-feedbacks-app-demo.gif'
 export const data: MyProjectsListProps[] = [
     {
         title: 'feedback_widget_title',
         about: 'feedback_widget_about',
         technologies: ['Typescript', 'React', 'React Hooks', 'Axios', 'Headlessui/react', 'classNames', 'firebase', 'phosphor-react', 'html2canvas', 'tailwind', 'react-router-dom',
             'Node', 'Express', 'Cors', 'Postgres', 'Prisma',],
-        demonstration: undefined,
+        demonstration: feedbackDemonstrationGif,
         linkGithub: 'https://github.com/italocc-git/nlw-return-impulse',
         linkPublicate: 'https://nlw-return-impulse-lake-six.vercel.app/'
 
